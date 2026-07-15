a7185c1733cf7bc599e395ced73d75739e916edd
nahuelcosta <nahuelalejandrocosta@gmail.com>
2026-07-14 21:43:25 -0300

    agrego java spring

---
+++ src/test/java/com/gymtrack/backend/GymtrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymtrack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
