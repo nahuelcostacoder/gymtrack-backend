cf13638ca2187b0ffc31bcd39f7f721b07c333da
nahuelcosta <nahuelalejandrocosta@gmail.com>
2026-07-27 18:41:19 -0300

    implementa dependencia swagger

--- target/generated-sources/annotations/com/gymtrack/backend/mapper/RutinaEjercicioMapperImpl.java
+++ target/generated-sources/annotations/com/gymtrack/backend/mapper/RutinaEjercicioMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-27T09:36:07-0300",
+    date = "2026-07-27T18:26:43-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.11 (Microsoft)"
 )
 @Component
